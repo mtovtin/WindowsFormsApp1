705ed095f2a8e995fbb38d70e86d11916319c0cb
mtovtin <49452703+mtovtin@users.noreply.github.com>
2021-07-04 13:34:04 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resbutton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.inputbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // resbutton
+            // 
+            this.resbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resbutton.Location = new System.Drawing.Point(343, 389);
+            this.resbutton.Name = "resbutton";
+            this.resbutton.Size = new System.Drawing.Size(116, 36);
+            this.resbutton.TabIndex = 0;
+            this.resbutton.Text = "Show Result";
+            this.resbutton.UseVisualStyleBackColor = true;
+            this.resbutton.Click += new System.EventHandler(this.resbutton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(145, 147);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(509, 213);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(341, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // inputbutton
+            // 
+            this.inputbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputbutton.Location = new System.Drawing.Point(343, 88);
+            this.inputbutton.Name = "inputbutton";
+            this.inputbutton.Size = new System.Drawing.Size(116, 36);
+            this.inputbutton.TabIndex = 3;
+            this.inputbutton.Text = "OK";
+            this.inputbutton.UseVisualStyleBackColor = true;
+            this.inputbutton.Click += new System.EventHandler(this.inputbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.Location = new System.Drawing.Point(299, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Please write number of numbers:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputbutton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.resbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button resbutton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button inputbutton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void inputbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView1.ColumnCount = int.Parse(textBox1.Text);
+                dataGridView1.RowCount = 2;
+                dataGridView1.Rows[0].HeaderCell.Value = "a";
+                dataGridView1.Rows[1].HeaderCell.Value = "b";
+                dataGridView1.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Enter a valid number!");
+            }
+
+            dataGridView1.AllowUserToAddRows = false;
+           
+            int colNumber = 1;
+            foreach(DataGridViewColumn col in dataGridView1.Columns)
+            {
+        
+                col.HeaderText = colNumber.ToString();
+                colNumber = colNumber + 1;
+            }
+          
+        }
+
+        /// <summary>
+        /// This function validates user's input. It only allows numeric values.
+        /// </summary>
+        /// <param name="dataGridView1"></param>
+        /// <param name="valuesA"></param>
+        /// <param name="valuesB"></param>
+        /// <returns>Lists containing user's numbers</returns>
+
+        private bool Validation(DataGridView dataGridView1,out List<double> valuesA, out List<double> valuesB)
+        {
+             valuesA = new List<double>();
+             valuesB = new List<double>();
+         
+
+            foreach (DataGridViewCell cell in dataGridView1.Rows[0].Cells)
+            {
+                try
+                {
+                    valuesA.Add(double.Parse(cell.Value.ToString()));
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show($"Invalid data. Try again!");
+                    return false;
+                }
+            }
+            foreach (DataGridViewCell cell in dataGridView1.Rows[1].Cells)
+            {
+                try
+                {
+                    valuesB.Add(Convert.ToDouble(cell.Value.ToString()));
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show($"Invalid data. Try again!");
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+
+        private void resbutton_Click(object sender, EventArgs e)
+        {
+            List<double> valuesMultiplied = new List<double>();
+            List<double> valuesA = new List<double>();
+            List<double> valuesB = new List<double>();
+            if (Validation(dataGridView1, out valuesA, out valuesB))
+            {
+
+                for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                {
+                    valuesMultiplied.Add(valuesA[i] * valuesB[i]);
+                }
+
+                var most = valuesMultiplied.GroupBy(i => i).OrderByDescending(grp => grp.Count()).Select(grp => grp.Key).First();
+                MessageBox.Show($"The most occuring element of multiplied arrays (if none show the first element) is: {most}", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
